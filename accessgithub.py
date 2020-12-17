7186404c73a7966d434f58d9f4b5dcb9e1948cca
Rosie Connolly <60625969+connolr3@users.noreply.github.com>
2020-12-17 20:48:31 +0000

    Add files via upload

--- accessgithub.py
+++ accessgithub.py
@@ -3,7 +3,8 @@ import json
 username = 'connolr3'
 
 # from https://github.com/user/settings/tokens
-token = 'c56bcd3455453fb6d266e72b0b8611084a519edc'
+# authentication to GitHub when using the GitHub API
+token = '4522a920d8697c0bf680ebe7fe50ccbd96f2df05'
 
 # create a re-usable session object with the user creds in-built
 gh_session = requests.Session()
@@ -14,7 +15,7 @@ repos_url = 'https://api.github.com/user/repos'
 repos = json.loads(gh_session.get(repos_url).text)
 
 # print the repo names
-
+print ("Repo Names")
 i = 0
 for repo in repos:
     print (repos[i]['name'])
@@ -24,10 +25,11 @@ for repo in repos:
 user_url = 'https://api.github.com/user'
 user = json.loads(gh_session.get(user_url).text)
 
-print (user["name"])
-print (user["login"])
-print (user["url"])
-print (user["bio"])
-print (user["following"])
-print (user["created_at"])
+#PRINT USER DETAILS
+print ("Name: "+user["name"])
+print ("Username: "+user["login"])
+print ("URL: "+user["url"])
+print ("Bio: "+user["bio"])
+print ("Following (no. accounts):" + str(user["following"]))
+print ("Creation Date: "+user["created_at"])
 
